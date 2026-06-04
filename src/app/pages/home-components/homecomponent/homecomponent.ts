33b5d1417190f124bc5bbfc9feb7d0e63f3df910
akhil <devandlaakhil@gmail.com>
2026-06-04 21:50:47 +0530

    maps added

--- src/app/pages/home-components/homecomponent/homecomponent.ts
+++ src/app/pages/home-components/homecomponent/homecomponent.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Subject, takeUntil } from 'rxjs';
@@ -15,6 +15,8 @@ import { TranslatePipe } from '../../../pipes/translatepipe-pipe';
 import { Location } from '../../../constants/enums/ad-posting-enums';
 import { CITY_COORDINATES } from '../../../constants/location-coordinates';
 import { LoaderServices } from '../../../shared-services/loader-services';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { GoogleMap, MapMarker, MapInfoWindow } from '@angular/google-maps';
 
 @Component({
   selector: 'app-homecomponent',
@@ -27,7 +29,11 @@ import { LoaderServices } from '../../../shared-services/loader-services';
     MatSelectModule,
     MatButtonModule,
     MatIconModule,
-    TranslatePipe
+    TranslatePipe,
+    GoogleMapsModule,
+    GoogleMap,
+    MapMarker,
+    MapInfoWindow,
   ],
   templateUrl: './homecomponent.html',
   styleUrl: './homecomponent.css',
@@ -42,41 +48,55 @@ export class Homecomponent implements OnInit {
   selectedType: string = '';
   selectedBudget: string = '';
   searchControl = new FormControl('');
-  searchEntered:boolean = false;
+  searchEntered: boolean = false;
   authService = inject(AuthService);
-  userId:string = '';
+  userId: string = '';
   cities = Object.values(Location);
   loaderService = inject(LoaderServices);
+  center: any;
+  zoom: number = 0;
+
+  selectedProperty: any;
+  @ViewChild(MapInfoWindow)
+  infoWindow!: MapInfoWindow;
 
   ngOnInit(): void {
     this.userId = this.authService.getUser()?.id;
     this.loadProperties();
   }
 
+  mapOptions: google.maps.MapOptions = {
+    disableDefaultUI: true,
+    zoomControl: true,
+    streetViewControl: false,
+    mapTypeControl: false,
+    fullscreenControl: false,
+  };
+
   getAllProperites(lat?: number, lng?: number): void {
-  this.loaderService.show();
-  this.realEstateApiSrv
-    .getAllList(lat, lng)
-    .pipe(takeUntil(this.destroy$))
-    .subscribe({
-      next: (res) => {
-        this.properties = res.data;
-        this.filteredProperties = [...this.properties];
-        this.setupSearch();
-        this.cdr.detectChanges();
-        this.loaderService.hide();
-      },
-      error: () => {
-        this.toastr.error('Something went wrong', 'Fail');
-        this.loaderService.hide();
-      },
-    });
-}
+    this.loaderService.show();
+    this.realEstateApiSrv
+      .getAllList(lat, lng)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (res) => {
+          this.properties = res.data;
+          this.filteredProperties = [...this.properties];
+          this.setupSearch();
+          this.cdr.detectChanges();
+          this.loaderService.hide();
+        },
+        error: () => {
+          this.toastr.error('Something went wrong', 'Fail');
+          this.loaderService.hide();
+        },
+      });
+  }
 
   setupSearch() {
     this.searchControl.valueChanges.subscribe((value) => {
       const searchText = (value || '').toLowerCase().trim();
-      searchText != "" ? this.searchEntered = true : this.searchEntered = false;      
+      searchText != '' ? (this.searchEntered = true) : (this.searchEntered = false);
       this.filteredProperties = this.properties.filter(
         (property) =>
           property.title?.toLowerCase().includes(searchText) ||
@@ -99,13 +119,17 @@ export class Homecomponent implements OnInit {
 
   onTypeChange(type: string) {
     this.selectedType = type;
-    this.selectedType != "" ||  this.selectedBudget != "" ? this.searchControl.disable(): this.searchControl.enable();
+    this.selectedType != '' || this.selectedBudget != ''
+      ? this.searchControl.disable()
+      : this.searchControl.enable();
     this.filterProperties();
   }
 
   onBudgetChange(budget: string) {
     this.selectedBudget = budget;
-    this.selectedBudget != "" || this.selectedType != "" ? this.searchControl.disable(): this.searchControl.enable();
+    this.selectedBudget != '' || this.selectedType != ''
+      ? this.searchControl.disable()
+      : this.searchControl.enable();
     this.filterProperties();
   }
 
@@ -133,39 +157,50 @@ export class Homecomponent implements OnInit {
     });
   }
 
-  onLocationChange(city:string):void{
-    if(city == ""){
+  onLocationChange(city: string): void {
+    if (city == '') {
       this.getAllProperites();
-    }else{
+    } else {
       const coords = CITY_COORDINATES[city];
       this.getAllProperites(coords.lat, coords.lng);
     }
   }
 
   loadProperties(): void {
-  if (!navigator.geolocation) {
-    console.warn('Geolocation is not supported by this browser.');
-    this.getAllProperites();
-    return;
+    if (!navigator.geolocation) {
+      console.warn('Geolocation is not supported by this browser.');
+      this.getAllProperites();
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        // Update map center
+        this.center = {
+          lat,
+          lng,
+        };
+        this.zoom = 15;
+        this.getAllProperites(lat, lng);
+      },
+      (error) => {
+        console.error('Location Error:', error);
+
+        // If user denies permission or location fails
+        this.getAllProperites();
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 60000,
+      },
+    );
   }
 
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      const lat = position.coords.latitude;
-      const lng = position.coords.longitude;
-      this.getAllProperites(lat, lng);
-    },
-    (error) => {
-      console.error('Location Error:', error);
-
-      // If user denies permission or location fails
-      this.getAllProperites();
-    },
-    {
-      enableHighAccuracy: true,
-      timeout: 10000,
-      maximumAge: 60000,
-    }
-  );
-}
+  openInfo(marker: MapMarker, property: any) {
+    this.selectedProperty = property;
+    this.infoWindow.open(marker);
+  }
 }
